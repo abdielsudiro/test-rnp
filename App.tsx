d13b6f2a093c4c17550f0e4be1fec772fe58ae0e
Inline Assembly <50492010+abdielsudiro@users.noreply.github.com>
2023-07-03 22:12:04 +0700

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Provider as PaperProvider} from 'react-native-paper';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {theme} from './src/Theme';
+import {AuthProvider} from './src/hooks/AuthProvider';
+import Navigation from './src/Navigation';
+
+// Create a react-query client
+const queryClient = new QueryClient();
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <PaperProvider theme={theme}>
+          <Navigation />
+        </PaperProvider>
+      </AuthProvider>
+    </QueryClientProvider>
+  );
+};
+
+export default App;
